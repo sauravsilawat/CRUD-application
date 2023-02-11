80c954d188a03a35b420698a07b610a2146c22e6
Saurav <saurav786silawat@gmail.com>
2023-02-11 13:05:29 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const connectDB = require('./server/database/connection');
+
+const app = express();
+
+dotenv.config({path: 'config.env'})
+const PORT = process.env.PORT || 8080
+
+// log request
+app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
+
+// parse request to the body parser
+app.use(bodyparser.urlencoded({extended : true}))
+
+// set view engine 
+app.set("view engine", "ejs")
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+// load routers
+app.use('/', require('./server/routes/router'))
+
+app.listen(3000, ()=>{console.log(`Server is running on http://Localhost:${3000}`)})
